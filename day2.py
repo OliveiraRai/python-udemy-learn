e43014b9f770935b4cde9418208e445784fb65fb
rai <rai.conslog@hotmail.com>
2026-06-03 20:14:43 -0300

    add: first commit

---
+++ day2.py
@@ -0,0 +1,54 @@
+### subscripting
+# print("Hello"[0]) # returns 'H'
+
+### string
+# print("123" + "456") # returns '123456' 
+
+### integer 
+# print(123 + 456) # returns '479' 
+
+### large integers
+# print(123_456_789) # returns 123456789 (this is cool i didnt know that)
+
+### float / FLOATing point number
+# print(3.141598) # returns '3.141598'
+
+### boolean
+# print(True) # returns 'True'
+# print(False) # returns 'False'
+
+### data types
+# print(type("Hello")) # returns "<class 'str'>""
+# print(type(123)) # returns "<class 'int'>""
+# print(type(3.141598)) # returns "<class 'float'>"
+# print(type(False)) # returns "<class 'bool'>"
+# print(f"Number of letters in your name: {len(input("Enter your name: "))}")
+
+### operations
+# print(123 + 456) # returns 579
+# print(456 - 123) # returns 333
+# print(1 * 2) # returns 2
+# print(2 / 2) # returns 1.0 ('/' always return a float)
+# print(3 // 2) # returns 1 ('//' does the same steps as '/', but then it removes any decimals)
+# #(2 ** 2) # returns 4
+## order of operations on a equation 
+## PEMDAS
+# P = parenthesis
+# E = exponents
+# M = multiplication
+# D = division
+# A = Addition
+# S = Subtraction
+# P -> E -> M/D -> A/S
+# print(3 * (3 + 3) / 3 - 3)
+
+### EXERCISE
+# print("######################")
+# print("### TIP CALCULATOR ###")
+# print("######################")
+# total = float(input("What was the total bill? R$"))
+# tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+# split = int(input("How many people to split the bill? "))
+# tip = total * (tip / 100)
+# total_per_person = (total + tip) / split
+# print(f"Each person should pay: R${total_per_person:.2f}")
