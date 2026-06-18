feb0be647bb029b21ff7cf033dbc686617431cd1
rai <rai.conslog@hotmail.com>
2026-06-17 22:35:55 -0300

    update: made caesar cypher logic

--- day8.py
+++ day8.py
@@ -50,15 +50,36 @@
 
 ### Caesar Cypher
 def caesar_cypher():
+    source = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+              "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", 
+              "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", 
+              "Z"]
+
     print("Welcome to the Caesar Cypher Encoder/Decoder")
-    enc_dec = input("Type the option number:\n  1. Encrypt\n  2. Decrypt\nChoice: ")
+    enc_dec = int(input("Type the option number:\n  1. Encrypt\n  2. Decrypt\nChoice: "))
     message = input("Type your message:\n  ")
-    shift_number = input("Type the shift number\n  ")
+    shift_number = int(input("Type the shift number\n  "))
 
     if enc_dec == 1:
-        print("You chose to enconde.")
+        enc_message = list(message)
+
+        for index, letter in enumerate(enc_message):
+            if letter.isalpha() and letter in source:
+                # index() finds the index of an already know value in the list
+                new_letter = source.index(f'{letter}') + shift_number
+                enc_message[index] = source[new_letter]
+        # "".join(something) will make list to string, and whetever is inside "here"
+        # will serve as the separator for each item in the list
+        print(f"The encoded message will be: {"".join(enc_message)}")
+        
     elif enc_dec == 2:
-        print("You chose to decode.")
+        dec_message = list(message)
+
+        for index, letter in enumerate(dec_message):
+            if letter.isalpha() and letter in source:
+                new_letter = source.index(f'{letter}') - shift_number
+                dec_message[index] = source[new_letter]
+        print(f"The decoded message will be: {"".join(dec_message)}")
     else:
         print("Please, choose a valid option.")
 
