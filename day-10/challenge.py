41a7ae71ef0f26ecdfb1f1a4c38fbf9f1f5851a6
rai <rai.conslog@hotmail.com>
2026-06-23 23:11:08 -0300

    add: logica do desafio do dia 10

---
+++ day-10/challenge.py
@@ -0,0 +1,55 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+# times = operations["*"]
+# print(times(4, 8)) # Esse foi o que eu fiz
+# print(operations["*"](4, 8)) # Esse foi o que a professora fez. Bem melhor.
+# Eu podia ter feito o que eu pensei nas linhas 20-23 do arquivo practice.py
+# operations["*"] pode ser lido também como 'add' apenas.
+# em linha, ficaria:
+# add
+# e para chamar a função em si, faríamos:
+# add()
+# ou seja, 'operations["*"]()' faz sentido. Só me resta entender a ponto de enxergar mais.
+
+print("Welcome to the best calculator out there.")
+while True:
+    will_continue = "y"
+    n1 = int(input("Enter a number: "))
+    while will_continue == "y":
+        operator_choice = input("Choose one between the four operations:\n  +\n  -\n  *\n  /\nChoice: ")
+        n2 = int(input("Enter another number: "))
+        if n2 == 0: # Isso virou uma validação sem querer haha
+            print(f"The calculation between {n1} and 0 is undefined.")
+            continue
+        if operator_choice == "+":
+            print(f"{n1} {operator_choice} {n2} = {operations["+"](n1, n2)}")
+            n1 = operations["+"](n1, n2)
+        elif operator_choice == "-":
+            print(f"{n1} {operator_choice} {n2} = {operations["-"](n1, n2)}")
+            n1 = operations["-"](n1, n2)
+        elif operator_choice == "*":
+            print(f"{n1} {operator_choice} {n2} = {operations["*"](n1, n2)}")
+            n1 = operations["*"](n1, n2)
+        elif operator_choice == "/":
+            print(f"{n1} {operator_choice} {n2} = {operations["/"](n1, n2)}")
+            n1 = operations["/"](n1, n2)
+        else:
+            print("Please, choose a valid option.")
+        will_continue = input("If you want to continue with this result, type 'y'. If you want to start a new calculation, type 'n'.\n  ")
