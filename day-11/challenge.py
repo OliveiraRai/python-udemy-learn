ad97aeecadfaf27ac269380a372a2b8de5595643
rai <rai.conslog@hotmail.com>
2026-06-25 00:42:35 -0300

    refactor: refiz a logica do blackjack

--- day-11/challenge.py
+++ day-11/challenge.py
@@ -2,28 +2,46 @@ import random
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 wanna_play = False
-choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-if choice == 'y':
-    wanna_play = True
-while wanna_play == True:
-    p_hand = random.sample(cards, 2)
-    c_hand = random.sample(cards, 2)
-    p_score = sum(p_hand)
-    c_score = sum(c_hand)
-    print(f"  Your cards: {p_hand}, current score: {p_score}")
-    print(f"  Computer's first card: {c_hand[0]}")
-    wanna_continue = input("Type 'y' to get another card, type 'n' to pass: ")
-    if wanna_continue == 'y':
-        p_hand.append(random.choice(cards))
+
+def blackjack():
+    choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    if choice == 'y':
+        p_hand = random.sample(cards, 2)
+        c_hand = random.sample(cards, 2)
         p_score = sum(p_hand)
+        c_score = sum(c_hand)
         print(f"  Your cards: {p_hand}, current score: {p_score}")
         print(f"  Computer's first card: {c_hand[0]}")
-        if p_score > 21:
-            print("You went over. You lose.")
-            choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-            continue
-    elif wanna_continue == 'n':
-        print("conta ota kk")
-    else:
-        print("Please, choose a valid option.")
-    wanna_play = False
+        wanna_continue = input("Type 'y' to get another card, type 'n' to pass: ")
+        if wanna_continue == 'n':
+            while c_score < 17:
+                c_hand.append(random.choice(cards))
+                c_score = sum(c_hand)
+                if c_hand[-1] == 11 and c_score > 21:
+                    c_hand[-1] = 1
+                    c_score = sum(c_hand)
+            print(f"  Your final hand: {p_hand}, final score: {p_score}")
+            print(f"  Computer's final hand: {c_hand}, final score: {c_score}")
+            if p_score > 21:
+                print("You went over. You lose.")
+                blackjack()
+            elif c_score > 21:
+                print("Opponent went over. You win.")
+                blackjack()
+            elif len(p_hand) == 2 and p_score == 21:
+                print("Win with a Blackjack")
+                blackjack()
+            elif (len(c_hand) == 2 and c_score == 21):
+                print("Oponent wins with a Blackjack")
+                blackjack()
+            elif p_score > c_score:
+                print("You win.")
+                blackjack()
+            elif c_score > p_score:
+                print("You lose.")
+                blackjack()
+            else:
+                print("Draw")
+                blackjack()
+    
+blackjack()
