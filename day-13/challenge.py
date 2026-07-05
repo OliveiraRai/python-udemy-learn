9ec116673f6b4823ffb4461d75208335401c5070
rai <rai.conslog@hotmail.com>
2026-07-05 00:34:29 -0300

    add: conclui lógica do desafio

--- day-13/challenge.py
+++ day-13/challenge.py
@@ -84,10 +84,60 @@ def verify_duplicity(person1, person2):
         person2 = person_picker()
     return person2
 
-def higher_lower():
+def higher_lower(logo, vs):
+    game_score = 0
     person1 = person_picker()
-    person2 = verify_duplicity(person1, person_picker())
-    print(person1)
-    print(person2)
-    
-higher_lower()
+    print(logo)
+    while True:
+        person2 = verify_duplicity(person1, person_picker())
+        print(f"Compare A: {person1["name"]}, a {person1["description"]}, from {person1["country"]}")
+        print(vs)
+        print(f"Against B: {person2["name"]}, a {person2["description"]}, from {person2["country"]}")
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower() 
+        while guess == "":
+            print("\n  Your guess cannot be blank.")
+            guess = input("Who has more followers? Type 'A' or 'B': ").lower() 
+        if person1["follower_count"] > person2["follower_count"] and guess == 'a':
+            game_score += 1
+            print('/n' * 20)
+            print(logo)
+            print(f"  You're right! Current score: {game_score}")
+            continue
+        elif person2["follower_count"] > person1["follower_count"] and guess == 'b':
+            game_score += 1
+            print('/n' * 20)
+            print(logo)
+            print(f"  You're right! Current score: {game_score}")
+            continue
+        else:
+            print("\n" * 20)
+            print(logo)
+            print(f"  Sorry, that's wrong. Final score: {game_score}")
+            break
+        
+def menu():
+    logo = r"""  _  _ _      _            
+ | || (_)__ _| |_  ___ _ _ 
+ | __ | / _` | ' \/ -_) '_|
+ |_||_|_\__, |_||_\___|_|  
+ | |   _|___/__ _____ _ _  
+ | |__/ _ \ V  V / -_) '_| 
+ |____\___/\_/\_/\___|_|   
+                           """
+    vs = r""" __   __    
+ \ \ / /__  
+  \ V (_-<_ 
+   \_//__(_)
+            """
+    while True:
+        choice = input("Do you want to play a round of Higher Lower? Type 'yes' or 'no': ").lower()
+        if choice == 'yes':
+            print("\n" * 20)
+            higher_lower(logo, vs)
+        elif choice == 'no':
+            print("\n  Come back anytime!")
+            break
+        else:
+            print("\n  Please, write a valid option.")
+        
+menu()
