f3d74e2ddba78f9a565a52f3e677699480606194
maou <rai.conslog@hotmail.com>
2026-08-03 22:12:32 -0300

    add: estudos sobre POO

--- day-16/practice.py
+++ day-16/practice.py
@@ -1,10 +1,43 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-turtle = Turtle()
-turtle.shape("turtle")
-turtle.color("chartreuse4")
-turtle.forward(15)
+# turtle = Turtle()
+# turtle.shape("turtle")
+# turtle.color("chartreuse4")
+# turtle.forward(15)
 
-my_screen = Screen()
-my_screen.window_height()
-my_screen.exitonclick()
+# my_screen = Screen()
+# my_screen.window_height()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+pokedex = [
+    {
+        "Pokemon": "Squirtle", 
+        "Type": "Water",
+    },
+    {
+        "Pokemon": "Charmander", 
+        "Type": "Fire",
+    },
+    {
+        "Pokemon": "Bulbasaur", 
+        "Type": "Plant",
+    },
+    {
+        "Pokemon": "Cyndaquil", 
+        "Type": "Fire",
+    },
+    {
+        "Pokemon": "Mudkip", 
+        "Type": "Water",
+    },
+]
+
+table = PrettyTable()
+for x in range(1):
+    table.add_column("Pokemon", [pokemon["Pokemon"] for pokemon in pokedex])
+    table.add_column("Type", [pokemon["Type"] for pokemon in pokedex])
+
+table.align = 'l'
+print(table)
---
+++ day-16/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+pokemons = [pokemon["Pokemon"] for pokemon in pokedex]
+# types = [pokemon["Type"] for pokemon in pokedex]
