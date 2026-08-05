5ae89e761e314cf80e63a68933f3fc97ec4de6e7
maou <rai.conslog@hotmail.com>
2026-08-05 15:38:02 -0300

    add: implementa método is_resource_sufficient

--- day-16/challenge/coffee_maker.py
+++ day-16/challenge/coffee_maker.py
@@ -1,18 +1,22 @@
-from menu import resources, MenuItem
+import menu
 
 class CoffeeMaker:
     def report():
-        for resource in resources:
+        for resource in menu.resources:
             match resource:
                 case "Water":
-                    print(f"{resource}: {resources[resource]}ml")
+                    print(f"{resource}: {menu.resources[resource]}ml")
                 case "Milk":
-                    print(f"{resource}: {resources[resource]}ml")
+                    print(f"{resource}: {menu.resources[resource]}ml")
                 case "Coffee":
-                    print(f"{resource}: {resources[resource]}g")    
+                    print(f"{resource}: {menu.resources[resource]}g")    
 
-    def is_resource_sufficient(drink: MenuItem):
+    def is_resource_sufficient(drink: menu.MenuItem):
+        for ingredient in drink.ingredients:
+            if menu.resources[ingredient] < drink.ingredients[ingredient]:
+                return False
+        return True
+
+    def make_coffee(order: menu.MenuItem):
         pass # TODO
 
-    def make_coffee(order: MenuItem):
-        pass # TODO
